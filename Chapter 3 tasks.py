e2b07874be7e19016c911a2125780d55c0553dbb
nurbek2405 <nurbekorazov@gmail.com>
2022-10-08 21:56:22 +0600

    Обновление + 2 задачи

--- Chapter 3 tasks.py
+++ Chapter 3 tasks.py
@@ -1,10 +1,12 @@
 #--------6.1---------------------------
+print('\n1 задача') 
 i=10
 if i > 1:
     i = i -1
 print(i)
 
 #--------6.2----------------------------
+print('\n2 задача') 
 i=1
 if i>1:
     i=i-1
@@ -15,6 +17,7 @@ elif i==1:
     print(i)
 
 #--------6.3----------------------------
+print('\n3 задача') 
 i=int(-3)
 if i > 1:
     i = i-1
@@ -26,6 +29,7 @@ else:
     print(i**2)
 
 #--------6.4----------------------------
+print('\n4 задача ') 
 i=40
 
 if i <=0:
@@ -36,10 +40,37 @@ elif i < 0:
         print(i)
 elif i > 0:
     i= i - 10
-    print('Тадааам')
     if i <=0:
         print(i)
 else:
     print('error')
  
- #--------6.5----------------------------   
+  
+print('\n5 задача') 
+i=10
+if i>1 and i < 10:
+    i = i -1
+    print(i)
+elif i == 1 or i == 10:
+    i = i + 1
+    print(i)
+print(i+1)
+
+#--------6.6---------------------------- 
+print('\n6 задача ') 
+
+i = 4
+if i > 1 and i < 10:
+    i=i*2
+    print(i)
+    if i>1 and i < 10:
+        i = i*2
+        print(i)
+        if i>1 and i <10:
+            print(i*2)
+        else:
+            print(i+2)
+    else:
+        print (i+2)
+else:
+    print(i)
