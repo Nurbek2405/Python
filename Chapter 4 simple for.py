cabb451edf7664071b309d105edabe762b96c809
nurbek2405 <nurbekorazov@gmail.com>
2022-10-11 20:38:27 +0600

    Chapter 4 Пример цикла For

---
+++ Chapter 4 simple for.py
@@ -0,0 +1,5 @@
+# Пример цикла for
+
+numberfor = int(input('Ввести количество вывода порядковых натуральных цифр на экран: '))
+for out in range (0,numberfor): # default count 1 (0,number,1) or (0,number)
+    print(out)
