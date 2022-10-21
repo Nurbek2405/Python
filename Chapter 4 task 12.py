3cbc057221c8bc7156d9b1b6ab9028306b670bca
nurbek2405 <nurbekorazov@gmail.com>
2022-10-21 20:34:25 +0600

    Добавлен диалог и дробные части, убран выводы цифр

--- Chapter 4 task 12.py
+++ Chapter 4 task 12.py
@@ -1,12 +1,14 @@
-a = 52344
+
+a = float(input('Введите любое число, проверим делится ли она на 3: '))
+aold=a
 s = 0
 while a != 0:
     s+=a%10
-    print('s=',s)
+    #print('s=',s)
     a=(a-a%10)//10
-    print('a=',a)
+    #print('a=',a)
 if s/3 == int(s/3):
-    print('s=',s)
-    print(True)
+    #print('s=',s)
+    print('Число ',aold,' является делимым на 3')
 else:
-    print(False)
+    print('Число ',aold,' не является делимым на 3')
