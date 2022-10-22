4b51fd143c68ee0b48073d4423ba948a93223a4f
nurbek2405 <nurbekorazov@gmail.com>
2022-10-22 21:56:14 +0600

    Добавлены примеры модуля random

---
+++ Chapter 5 simple rand int and range.py
@@ -0,0 +1,15 @@
+# Пример кода random and range
+import random
+
+print("Примеры модуля random.randint(2,15): ",random.randint(2,15))
+print("Примеры модуля random.randint(2,15): ",random.randint(2,15))
+print("Примеры модуля random.randint(2,15): ",random.randint(2,15))
+print("Примеры модуля random.randint(2,15): ",random.randint(2,15))
+print("Примеры модуля random.range(7): ",random.randrange(7))
+print("Примеры модуля random.range(3,17): ",random.randrange(3,17))
+print("Примеры модуля random.range(-13,0,2): ",random.randrange(-13,0,2))
+print("Примеры модуля random.range(33,-33,-3): ",random.randrange(33,-33,-3))
+print("Примеры модуля random.random(): ",random.random())
+print("Примеры модуля random.random(): ",random.random())
+print("Примеры модуля random.random(): ",random.random())
+print("Примеры модуля random.random(): ",random.random())
