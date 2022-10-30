2752ad95040f2909bb69bc4e859468ff88f6783c
nurbek2405 <nurbekorazov@gmail.com>
2022-10-30 22:31:45 +0600

    Глава 7, пример вывода массива

---
+++ Chapter 7 array simple list 1.py
@@ -0,0 +1,10 @@
+List1 = ['1', [1,2,3,'abc'],-2]
+
+print(List1)  # вывод всего массива
+print(List1[0])
+#print(List1[1]) вывод общего под массив
+print(List1[1][0])
+print(List1[1][1])
+print(List1[1][2])
+print(List1[1][3])
+print(List1[2])
