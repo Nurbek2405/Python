42009e60a6d6beedba755a8eab81117a2dc2390d
nurbek2405 <nurbekorazov@gmail.com>
2022-10-30 22:45:07 +0600

    Глава 7, примеры вывода массива через range

---
+++ Chapter 7 array simple list 2.py
@@ -0,0 +1,19 @@
+# range (a,b,c) Например:
+
+# Создание списка с помощью функции list()
+List2 = list('spam')
+print(List2)
+
+# Создание списка с помощью функции list() и range(a) 
+List3 = list(range(5))
+print(List3)
+
+# Создание списка с помощью функции list() и range(a,b)
+List4 = list(range(-2,3))
+print(List4)
+
+# Создание списка с помощью функции list() и range (a,b,c) 
+List5 = list(range(-20,50,15))
+print(List5)
+
+
