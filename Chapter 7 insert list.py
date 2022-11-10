81eb65c8c84b468d5b2d5fbe28dfc460e98971a1
nurbek2405 <nurbekorazov@gmail.com>
2022-11-10 23:55:09 +0600

    Добавлены изменение в задачу по удаление х

---
+++ Chapter 7 insert list.py
@@ -0,0 +1,15 @@
+# list.insert(i,x) 
+list10 = [1, 1, 'uu', 0, [26 , 1]]
+print(list10)
+list10.insert(2, [5])
+print(list10)
+
+
+list10 = [1, 1, [5], 'u', 0, [26,1] ]
+
+
+list10.remove(1)
+list10.remove(1)
+list10.remove(1)
+
+print(list10)
